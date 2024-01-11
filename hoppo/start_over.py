fb4bae634a198ad6db21f2c67139eddbc818bc08
Andrew Hearin <ahearin@anl.gov>
2024-01-11 15:03:01 -0600

    Getting close to scalar value of SFH params

--- hoppo/start_over.py
+++ hoppo/start_over.py
@@ -2,9 +2,9 @@
 """
 from collections import OrderedDict
 
-import numpy as np
 from jax import jit as jjit
 from jax import numpy as jnp
+from jax import random as jran
 
 from .pdf_mainseq import DEFAULT_SFH_PDF_MAINSEQ_PARAMS, _get_chol_params_mainseq
 from .pdf_mainseq import _get_cov_scalar as _get_cov_scalar_ms
@@ -21,8 +21,7 @@ from .pdf_quenched import _get_mean_smah_params_quench
 
 
 @jjit
-def draw_single_sfh_MIX_with_exsitu(
-    t_table,
+def mc_diffstar_u_params_singlegal(
     mah_params,
     p50,
     ran_key,
@@ -40,7 +39,7 @@ def draw_single_sfh_MIX_with_exsitu(
     cov_mainseq_pdict = OrderedDict(
         [(key, val) for (key, val) in pdf_pdict_MS.items() if "chol_" in key]
     )
-    mean_ms = _get_mean_smah_params_mainseq(lgm0, **means_mainseq_pdict)
+    mu_ms = _get_mean_smah_params_mainseq(lgm0, **means_mainseq_pdict)
     chol_params_ms = _get_chol_params_mainseq(lgm0, **cov_mainseq_pdict)
     cov_ms = _get_cov_scalar_ms(*chol_params_ms)
 
@@ -56,7 +55,7 @@ def draw_single_sfh_MIX_with_exsitu(
         [(key, val) for (key, val) in pdf_pdict_Q.items() if "chol_" in key]
     )
 
-    mean_q = _get_mean_smah_params_quench(lgm0, **means_q_pdict)
+    mu_q = _get_mean_smah_params_quench(lgm0, **means_q_pdict)
     chol_params_q = _get_chol_params_quench(lgm0, **cov_q_pdict)
     cov_q = _get_cov_scalar_q(*chol_params_q)
 
@@ -64,4 +63,26 @@ def draw_single_sfh_MIX_with_exsitu(
     R_vals_q = _get_slopes_quench(lgm0, *R_model_params_q)
     shifts_q = jnp.array(R_vals_q) * (p50 - 0.5)
 
-    return mean_ms, cov_ms, shifts_ms, mean_q, cov_q, shifts_q
+    # This part is not implemented yet either, but we need a scalar value of F_q
+    # Compute quenched fraction
+    # frac_quench = frac_quench_vs_lgm0(lgm0, fquench_x0, *pdf_parameters_Q[1:4])
+
+    # Monte Carlo draws
+    ms_key, q_key, frac_q_key = jran.split(ran_key, 3)
+
+    u_params_q = jran.multivariate_normal(q_key, jnp.array(mu_q), cov_q, shape=())
+    u_params_ms = jran.multivariate_normal(q_key, jnp.array(mu_ms), cov_ms, shape=())
+
+    u_params_ms = u_params_ms + shifts_ms
+
+    # Next line fails because shifts_q.shape=(9,) u_params_q.shape=(8,)
+    # u_params_q = u_params_q + shifts_q
+
+    uran = jran.uniform(frac_q_key, minval=0, maxval=1, shape=())
+
+    # Finally need to implement this properly
+    # should use the scalar value of F_q calculated above
+    # Need to take care of u_params_q and u_params_ms having different dimension
+    # u_params = jnp.where(uran < frac_q, u_params_q, u_params_ms)
+
+    return u_params_q, u_params_ms, shifts_q
--- hoppo/tests/test_start_over.py
+++ hoppo/tests/test_start_over.py
@@ -1,6 +1,5 @@
 """
 """
-import numpy as np
 from diffmah.defaults import DEFAULT_MAH_PARAMS
 from jax import random as jran
 
@@ -9,11 +8,7 @@ from .. import start_over
 
 def test_something():
     ran_key = jran.PRNGKey(0)
-    t0 = 13.7
-    nt = 100
-    t_table = np.linspace(0.1, t0, nt)
 
     p50 = 0.25
-    args = t_table, DEFAULT_MAH_PARAMS, p50, ran_key
-    _res = start_over.draw_single_sfh_MIX_with_exsitu(*args)
-    mean_ms, cov_ms, shifts_ms, mean_q, cov_q, shifts_q = _res
+    args = DEFAULT_MAH_PARAMS, p50, ran_key
+    _res = start_over.mc_diffstar_u_params_singlegal(*args)
